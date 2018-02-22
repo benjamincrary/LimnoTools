8babe8267e22cc97aa94a2c340c1347a1a2b6c24
benjamincrary <benjamin.crary@gmail.com>
2018-02-22 15:04:46 -0600

    Added depthType
    created the depthType function to identify surface/subsurface samples

---
+++ R/depthType-function.R
@@ -0,0 +1,15 @@
+#' Water Column Depth Designator (surface or subsurface) for samples
+#'
+#' This function returns a vector designating whether a sample is a surface or subsurface sample depending a single cutoff depth.
+#' @param startDepth, endDepth, cutoff
+#' @keywords water column, surface, subsurface
+#' @export
+#' @examples
+#' cat_function()
+
+
+
+depthType <- function(startDepth, endDepth, cutoff) {
+  ifelse(startDepth > cutoff | endDepth > cutoff, "subsurface", "surface")
+}
+
